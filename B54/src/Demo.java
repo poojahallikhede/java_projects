541c5f576bf3397a15410da98b87105f0a1b0e43
poojahallikhede <137510087+poojahallikhede@users.noreply.github.com>
2023-06-23 18:10:22 +0530

    Add files via upload

---
+++ B54/src/Demo.java
@@ -0,0 +1,16 @@
+/**
+ * @author Pooja
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lovely Day");
+
+	}
+
+}
